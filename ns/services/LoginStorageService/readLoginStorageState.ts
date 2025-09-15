b4e5c61f858bc9b6621fa1bbf89f8d3c45c64917
Vyacheslav Shimarulin <shimarulin@gmail.com>
2025-09-15 23:44:59 +0300

    fix(login): reading state file

--- ns/services/LoginStorageService/readLoginStorageState.ts
+++ ns/services/LoginStorageService/readLoginStorageState.ts
@@ -1,9 +1,17 @@
 import { readFile } from 'ags/file'
 
+import { getErrorMessage, toLog } from '@libs/log'
+
 import { STATE_FILE } from './constants'
 import { LoginStorageRecord } from './types'
 
 export function readLoginStorageState(): LoginStorageRecord | null {
-  const file = readFile(STATE_FILE)
-  return file.length > 0 ? JSON.parse(file) : null
+  try {
+    const file = readFile(STATE_FILE)
+    return file.length > 0 ? JSON.parse(file) : null
+  }
+  catch (err) {
+    toLog(getErrorMessage(err))
+    return null
+  }
 }
